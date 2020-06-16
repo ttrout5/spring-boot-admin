e0b2e32e5747f56f736929b175175b22e3b1b641
Tim Troutman <troutmantim@gmail.com>
2020-06-15 21:16:27 -0500

    Initial commit of spring boot admin server

---
+++ src/main/java/com/udemy/springboot/admin/SpringBootAdminApplication.java
@@ -0,0 +1,13 @@
+package com.udemy.springboot.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAdminApplication.class, args);
+	}
+
+}
